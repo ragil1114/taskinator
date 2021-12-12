d483c345afd2fb572710f35d11ddbbea962886de
Ragil14 <rahil.gilani@gmail.com>
2021-12-12 17:49:35 -0600

    added task item option

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,14 @@
-var buttonEl = document.querySelector("#save-task");
-console.log(buttonEl);
+    // The following two variables help reference the dynamically created elements to their respective ID tags.
+var buttonEl = document.querySelector("#save-task"); 
+var tasksToDoEl = document.querySelector("#tasks-to-do"); 
+
+    // function that creates a task item
+var createTaskHandler = function() { 
+  var listItemEl = document.createElement("li"); 
+  listItemEl.className = "task-item"; 
+  listItemEl.textContent = "This is a new task."; 
+  tasksToDoEl.appendChild(listItemEl); 
+}; 
+
+    // Call for createTaskHandler()
+buttonEl.addEventListener("click", createTaskHandler);
