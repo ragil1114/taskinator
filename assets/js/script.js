8fef3c21ec3c4d7f18d744e2efbad5168e17dcea
Ragil14 <rahil.gilani@gmail.com>
2021-12-13 15:22:31 -0600

    added variable & EventListener for formEl

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,15 @@
-    // The following two variables help reference the dynamically created elements to their respective ID tags.
-var buttonEl = document.querySelector("#save-task"); 
+var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do"); 
 
     // function that creates a task item
-var createTaskHandler = function() { 
+var createTaskHandler = function(event) {
+  event.preventDefault();
+  
   var listItemEl = document.createElement("li"); 
   listItemEl.className = "task-item"; 
   listItemEl.textContent = "This is a new task."; 
-  tasksToDoEl.appendChild(listItemEl); 
+  tasksToDoEl.appendChild(listItemEl);
 }; 
 
     // Call for createTaskHandler()
-buttonEl.addEventListener("click", createTaskHandler);
+formEl.addEventListener("submit", createTaskHandler);
